22fed476d2218e1b7a553b9a2665446704ceeade
Trainee (Mohammed Abdulla Mohammed Hassan Alhosani) <MAB52339@emiratesnbd.com>
2023-02-21 11:54:13 +0400

    Added checks to make sure machine doesnt dispense coin if it has 0 of it

--- api_server/src/modules/selectCoin.ts
+++ api_server/src/modules/selectCoin.ts
@@ -3,7 +3,7 @@ import coins from "../types/coins";
 export const selectCoin = (changeDue: number, machineCoins: coins, coinsDispensed: coins): number => {
 
     // 1 dollar
-    if(changeDue >= 100){
+    if(changeDue >= 100 && machineCoins.dollar1 > 0){
         changeDue -= 100;
         machineCoins.dollar1 -= 1;
         coinsDispensed.dollar1 += 1;
@@ -11,7 +11,7 @@ export const selectCoin = (changeDue: number, machineCoins: coins, coinsDispense
     }
 
     // 50 cents
-    if(changeDue >= 50){
+    if(changeDue >= 50 && machineCoins.cent50 > 0){
         changeDue -= 50;
         machineCoins.cent50 -= 1;
         coinsDispensed.cent50 += 1;
@@ -19,7 +19,7 @@ export const selectCoin = (changeDue: number, machineCoins: coins, coinsDispense
     }
 
     // 20 cents
-    if(changeDue >= 20){
+    if(changeDue >= 20 && machineCoins.cent20 > 0){
         changeDue -= 20;
         machineCoins.cent20 -= 1;
         coinsDispensed.cent20 += 1;
@@ -27,7 +27,7 @@ export const selectCoin = (changeDue: number, machineCoins: coins, coinsDispense
     }
 
     // 10 cents
-    if(changeDue >= 10){
+    if(changeDue >= 10 && machineCoins.cent10 > 0){
         changeDue -= 10;
         machineCoins.cent10 -= 1;
         coinsDispensed.cent10 += 1;
@@ -35,7 +35,7 @@ export const selectCoin = (changeDue: number, machineCoins: coins, coinsDispense
     }
 
     // 5 cents
-    if(changeDue >= 5){
+    if(changeDue >= 5 && machineCoins.cent5 > 0){
         changeDue -= 5;
         machineCoins.cent5 -= 1;
         coinsDispensed.cent5 += 1;
@@ -43,7 +43,7 @@ export const selectCoin = (changeDue: number, machineCoins: coins, coinsDispense
     }
 
     // 2 cents
-    if(changeDue >= 2){
+    if(changeDue >= 2 && machineCoins.cent2 > 0){
         changeDue -= 2;
         machineCoins.cent2 -= 1;
         coinsDispensed.cent2 += 1;
