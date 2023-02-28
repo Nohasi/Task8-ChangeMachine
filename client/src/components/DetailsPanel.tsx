f9b4b3c98d36a96d663f19b099594d305f275ffd
Trainee (Mohammed Abdulla Mohammed Hassan Alhosani) <MAB52339@emiratesnbd.com>
2023-02-28 12:46:31 +0400

    Began setting up details panel

---
+++ client/src/components/DetailsPanel.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface detailsProps {
+    errorStatus: boolean,
+    errorMessage: string
+}
+
+export const DetailsPanel = (props: detailsProps) => {
+    return (
+        <div className="container">
+
+        </div>
+    );
+}
